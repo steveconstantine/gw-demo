1ca85bdf5fc28e8934a259d6b6c761d57ca94844
steveconstantine <38266065+steveconstantine@users.noreply.github.com>
2018-11-09 02:06:52 -0800

    typos

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { graphql, compose } from 'react-apollo';
-import CartContainer from './containers/cartcontainer';
 import gql from 'graphql-tag';
 import {
   createCheckout,
